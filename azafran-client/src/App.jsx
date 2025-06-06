4b99bf88aa215594d5f022128c2eadab991529a6
bernat alcudia <bernatalcudia@gmail.com>
2025-06-06 10:24:36 +0200

    feat(app):create folder pages and create files Login.jsx and Register.jsx;style(app):create routes Login.jsx and Register.jsx with divs

--- azafran-client/src/App.jsx
+++ azafran-client/src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { Login } from './pages/Login/Login.jsx'
+import { Register } from './pages/Register/Register.jsx'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
