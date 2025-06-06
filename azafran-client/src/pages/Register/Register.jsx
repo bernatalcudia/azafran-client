f468cb887c74e7d8274d9c2c40c593fd8ffc0cef
bernat alcudia <bernatalcudia@gmail.com>
2025-06-06 10:51:38 +0200

    style(app):inprove view Register.jsx

--- azafran-client/src/pages/Register/Register.jsx
+++ azafran-client/src/pages/Register/Register.jsx
@@ -1,7 +1,13 @@
-
+import { Button, Flex, Input } from 'antd'
 const Register = () => {
     return (
-        <div>Register</div>
+        <Flex gap={'8px'} style={{ width: '400px' }} vertical>
+            <h1>Register</h1>
+            <Input placeholder='User'></Input>
+            <Input placeholder='Password'></Input>
+            <Input placeholder='Repeat password'></Input>
+            <Button type='primary'>Register</Button>
+        </Flex>
     )
 }
 
