a901ad82373942012c8fd294b9bac87688235888
bernat alcudia <bernatalcudia@gmail.com>
2025-06-10 10:25:14 +0200

    feat(app):implement Home.jsx route in app and complete Home.jsx file

--- azafran-client/src/App.jsx
+++ azafran-client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { Login } from './pages/Login/Login.jsx'
 import { Register } from './pages/Register/Register.jsx'
+import { Home } from './pages/Home/Home.jsx'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
       </Routes>
---
+++ azafran-client/src/pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+
+
+function Home() {
+    const [ingredients, setIngredients] = useState([])
+
+    useEffect(() => {
+        const accessToken = localStorage.getItem('accessToken')
+        fetch('http//localhost:8000/ingredients', {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: accessToken,
+            },
+        }).then((res) => res.json())
+    }).then((data) => {
+        console.log(data)
+    }, [])
+
+    return (
+        <div>Home</div>
+    )
+}
+
+export { Home }
