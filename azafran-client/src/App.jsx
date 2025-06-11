db98b72531d2c1fe710221bc23d9dbb5960eeba5
bernat alcudia <bernatalcudia@gmail.com>
2025-06-11 10:57:32 +0200

    feat(app):update routes in App.jsx with AuthedRoutes.jsx and UnAuthedRoutes.jsx

--- azafran-client/src/App.jsx
+++ azafran-client/src/App.jsx
@@ -1,18 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router'
-import { Login } from './pages/Login/Login.jsx'
-import { Register } from './pages/Register/Register.jsx'
-import { Home } from './pages/Home/Home.jsx'
+import { BrowserRouter } from 'react-router'
+import { UnAuthedRoutes } from '../routes/UnAuthedRoutes'
+import { AuthedRoutes } from '../routes/AuthedRoutes'
 import './App.css'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
+      <UnAuthedRoutes />
+      <AuthedRoutes />
     </BrowserRouter>
   )
 }
